e8325bdd4fba51aaf4fbf72f806c48b97ae7137d
Muhammad Hafizuddin Bin Nazlee <49903360+hfzdnnzl@users.noreply.github.com>
2021-08-19 14:10:17 +0800

    attempting to fix timeout error

--- ui.R
+++ ui.R
@@ -30,26 +30,26 @@ fluidPage(
     plotOutput('grades'),
     
     uiOutput("title4"),
-    tableOutput('table'),
+    tableOutput('table')
+  ),
   
-    # keep alive
-    tags$head(
-      HTML(
-        "
-    <script>
-    var socket_timeout_interval
-    var n = 0
-    $(document).on('shiny:connected', function(event) {
-    socket_timeout_interval = setInterval(function(){
-    Shiny.onInputChange('count', n++)
-    }, 15000)
-    });
-    $(document).on('shiny:disconnected', function(event) {
-    clearInterval(socket_timeout_interval)
-    });
-    </script>
-    "
-        )),
-    textOutput("keepAlive")
-  )
+  # keep alive
+  tags$head(
+    HTML(
+      "
+  <script>
+  var socket_timeout_interval
+  var n = 0
+  $(document).on('shiny:connected', function(event) {
+  socket_timeout_interval = setInterval(function(){
+  Shiny.onInputChange('count', n++)
+  }, 15000)
+  });
+  $(document).on('shiny:disconnected', function(event) {
+  clearInterval(socket_timeout_interval)
+  });
+  </script>
+  "
+      )),
+  textOutput("keepAlive")
 ) # fluidPage
