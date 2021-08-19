4e1bd967e36fce0e33886ef042be4c56d82cc991
Muhammad Hafizuddin Bin Nazlee <49903360+hfzdnnzl@users.noreply.github.com>
2021-08-19 12:51:48 +0800

    edit init.R

--- init.R
+++ init.R
@@ -1,10 +1,10 @@
 
 # install R packages
 
-my_packages = c('shiny','shinythemes','xlsx','stringr','dplyr','pdftools')
+my_packages = c('shinythemes','xlsx','stringr','dplyr','pdftools')
 
 install_if_missing = function(p){
-  if(p %in% rownames(install.packages()) == FALSE){
+  if(p %in% rownames(installed.packages()) == FALSE){
     install.packages(p)
   }
 }
